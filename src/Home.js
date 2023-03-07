c9a4fa8aead8ae5026d8d9a81134e33a285ec26b
nikita23421 <113423121+nikita23421@users.noreply.github.com>
2023-03-07 12:11:54 +0000

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+const[isPending,setISPending]=useState(true);
+const [error,setError] = useState(null);
+ 
+  useEffect(() => {
+   setTimeout(()=>{
+    fetch("http://localhost:8000/blogs")
+    .then((res) => {
+      if(!res.ok) {
+        throw Error('could not fetch the data for that resource')
+      }
+      return res.json();
+    })
+    .then((data) => {
+      setBlogs(data);
+      setISPending(false);
+      setError(null)
+    })
+    .catch(err=> {
+      setISPending(false);
+      setError(err.message);
+    })
+   }, 1000);
+  }, []);
+  return (
+    <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs  && <BlogList blogs={blogs} title="All Blogs!"  />}
+    </div>
+  );
+};
+
+export default Home;
